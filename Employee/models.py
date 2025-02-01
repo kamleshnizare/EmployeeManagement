3fae9452263492de846c1a0d5adc2f1637313834
kamleshnizare <kamleshnizare078@gmail.com>
2025-02-01 22:46:35 +0530

    create table again

--- Employee/models.py
+++ Employee/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class EmployeeInfo(models.Model):
+    ename = models.CharField(max_length=85)
+    eage = models.IntegerField()
+    esal = models.FloatField()
+    eadd = models.CharField(max_length=85)
